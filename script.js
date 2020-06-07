7732bb4e4c26014276c36e1a59daf69df4845cdc
paramshreeApt <66487862+paramshreeApt@users.noreply.github.com>
2020-06-07 10:48:35 +0530

    Updated changes

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
-$('#firstFloorBtn').click(function() {
-    $('#firstFloor').show()
-    $('#secondFloor').hide()
-    $('#thirdFloor').hide()
-    $('#fourthFloor').hide()
-    $('#fifthFloor').hide()
-})
-
-$('#secondFloorBtn').click(function() {
-
-    $('#secondFloor').show()
-    $('#firstFloor').hide()
-    $('#thirdFloor').hide()
-    $('#fourthFloor').hide()
-    $('#fifthFloor').hide()
-})
-
-$('#thirdFloorBtn').click(function() {
-
-    $('#secondFloor').hide()
-    $('#firstFloor').hide()
-    $('#thirdFloor').show()
-    $('#fourthFloor').hide()
-    $('#fifthFloor').hide()
-})
+$(document).ready(function() {
+    $("html, body").animate({ scrollTop: 0 }, "slow");
 
 
-$('#fourthFloorBtn').click(function() {
 
-    $('#secondFloor').hide()
-    $('#firstFloor').hide()
-    $('#thirdFloor').hide()
-    $('#fourthFloor').show()
-    $('#fifthFloor').hide()
-})
 
-$('#fifthFloorBtn').click(function() {
+    $('#firstFloorBtn').click(function() {
+        $('#firstFloor').show()
+        $('#secondFloor').hide()
+        $('#thirdFloor').hide()
+        $('#fourthFloor').hide()
+        $('#fifthFloor').hide()
+        $('#shop').hide()
+    })
 
-    $('#secondFloor').hide()
-    $('#firstFloor').hide()
-    $('#thirdFloor').hide()
-    $('#fourthFloor').hide()
-    $('#fifthFloor').show()
-})
+    $('#secondFloorBtn').click(function() {
+
+        $('#secondFloor').show()
+        $('#firstFloor').hide()
+        $('#thirdFloor').hide()
+        $('#fourthFloor').hide()
+        $('#fifthFloor').hide()
+        $('#shop').hide()
+    })
+
+    $('#thirdFloorBtn').click(function() {
+        $('#thirdFloor').show()
+        $('#secondFloor').hide()
+        $('#firstFloor').hide()
+
+        $('#fourthFloor').hide()
+        $('#fifthFloor').hide()
+        $('#shop').hide()
+    })
+
+
+    $('#fourthFloorBtn').click(function() {
+        $('#fourthFloor').show()
+        $('#secondFloor').hide()
+        $('#firstFloor').hide()
+        $('#thirdFloor').hide()
+
+        $('#fifthFloor').hide()
+        $('#shop').hide()
+    })
+
+    $('#fifthFloorBtn').click(function() {
+        $('#fifthFloor').show()
+        $('#secondFloor').hide()
+        $('#firstFloor').hide()
+        $('#thirdFloor').hide()
+        $('#fourthFloor').hide()
+
+        $('#shop').hide()
+    })
+
+    $('#shopBtn').click(function() {
+        $('#shop').show()
+
+        $('#secondFloor').hide()
+        $('#firstFloor').hide()
+        $('#thirdFloor').hide()
+        $('#fourthFloor').hide()
+        $('#fifthFloor').hide()
+    })
+
+
+
+
+
+
+});
